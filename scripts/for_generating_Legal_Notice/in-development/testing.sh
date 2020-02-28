4639a3d39c54a03c7e911f1850a6b26e2848a151
MarkFern <49532200+MarkFern@users.noreply.github.com>
2020-02-28 18:49:22 +0000

    Code now works..
    Converts JSON contributors list to list delimited by new line characters.

--- scripts/for_generating_Legal_Notice/in-development/testing.sh
+++ scripts/for_generating_Legal_Notice/in-development/testing.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-more Page_1.json |  sed -E 's/^[[:space:]]*\{([[:space:]]*\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"[[:space:]]*:[[:space:]]*(\{[[:space:]]*\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"[[:space:]]*:[[:space:]]*)*(\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"|[0-9]+)[[:space:]]*}?[[:space:]]*,?[[:space:]]*)+\"contributors\"[[:space:]]*:[[:space:]]*\[[[:space:]]*//i' | more 
+more Page_1.json |  sed -E 's/^[[:space:]]*\{([[:space:]]*\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"[[:space:]]*:[[:space:]]*(\{[[:space:]]*\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"[[:space:]]*:[[:space:]]*)*(\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"|[0-9]+)[[:space:]]*}?[[:space:]]*,?[[:space:]]*)+\"contributors\"[[:space:]]*:[[:space:]]*\[[[:space:]]*//i' | sed -E 's/([[:space:]]*(\]|\})[[:space:]]*)+$/\}/i'  | sed -E ':x s/^([[:space:]]*(([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\n)*[[:space:]]*)?[[:space:]]*,?[[:space:]]*\{[[:space:]]*\"userid\"[[:space:]]*:[[:space:]]*(\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"|[0-9]+)[[:space:]]*,?[[:space:]]*\"name\"[[:space:]]*:[[:space:]]*\"(([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+)\"[[:space:]]*\}[[:space:]]*/\1\8\n/i; t x'
 
 
 
 
+
+
+############################ NOTES ############################
+# RECURSIVE REPLACEMENT USING SED:  echo aaaaaaaaaaab | sed -E ':x s/(aab)/ab/ ; t x'
 
 
 # SED code for matching JSON string token,
@@ -14,9 +18,42 @@ more Page_1.json |  sed -E 's/^[[:space:]]*\{([[:space:]]*\"([^\"\\\cG\cH\cI\cJ\
 # Control characters escaped according to `Sed` specs: \x00 \cG \cH \cI \cJ \cK \cL \cM \cZ \c[
 #           ([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))
 
+# `Sed` code for converting usernames JSON into a simple list of usernames.
+sed -E ':x 		
+					s/
+							^([[:space:]]*
+												(([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\n)*
+							  [[:space:]]*)?
 
 
-############################ NOTES ############################
+							 [[:space:]]*
+							 ,?
+							 [[:space:]]*
+							 \{
+									[[:space:]]*
+											\"userid\"
+											[[:space:]]*
+											:
+											[[:space:]]*
+											(\"([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+\"|[0-9]+)
+									[[:space:]]*
+									,?
+									[[:space:]]*
+											\"name\"
+											[[:space:]]*
+											:
+											[[:space:]]*
+											\"
+												(([^\"\\\cG\cH\cI\cJ\cK\cL\cM\cZ\c[\x00]|\\([\"\\\/bfnrt]|u[0-9A-F]{4}))+)
+											\"
+											[[:space:]]*
+							\}
+							[[:space:]]*
+					  /
+							\1\8\n
+					  /i;
+			   t x'
+
 
 # REGULAR EXPRESSION FOR MATCHING BEGINNING OF JSON BEFORE CONTRIBUTOR NAMES BEGIN.
 # PRETTY-PRINTED FORM USING RECURSION:
